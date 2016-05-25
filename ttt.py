94aa400f1ae34f552e0244a48f6d8f0aac8738b3
johnrroesler <johnrroesler@gmail.com>
2016-05-25 17:05:47 -0500

    Add files via upload

--- ttt.py
+++ ttt.py
@@ -1,69 +1,140 @@
+import time
+import sys
+import random
 #http://www.practicepython.org/
-size = 3
+#define the variables needed for the program
 players = dict()
 players["1"] = "X"
 players["2"] = "O"
-players["0"] = " "
-game = list()
-game = [[0,0,0],[0,0,0],[0,0,0]]
-wincom1 = [1,1,1]
-wincom2 = [2,2,2]
-def bhorizon():
-    print (" ---") * size
-def bvert(var):
-    if var == 0 or var == 1 or var == 2:
-        if var == 0: lst = [1,2,3]
-        elif var == 1: lst = [3,4,5]
-        elif var == 2: lst = [5,6,7]
-        print ("| %s | %s | %s |" % (str(var+lst[0]),str(var+lst[1]),str(var+lst[2])))
-    else:
-        print ("| %s " % var )*size + "|"
+dic = dict()
+game = dict()
+wincom1 = ["X","X","X"]
+wincom2 = ["O","O","O"]
+turncount = 1
+#overall gameplay function - this starts and calls the appropriate functions
+def playgame(var):
+    initgame()
+    for i in range (1,10):
+        taketurn(var)
+        winner = chkwin()
+        if winner == "winner": break
+        var += 1
+    if winner != "winner": winner = "tie"
+    endgame(winner,var)
 #initialize game board
 def initgame():
-    print "\nThe positions on the game board are numbered as follows:"
-    for i in range(size):
-        bhorizon()
-        bvert(i)
-    bhorizon()
+    randprint("\nWelcome to Tic Tac Toe!\n")
+    randprint("This is a game for two players, X and O, \nwho take turns marking the spaces in a 3x3 grid. \nThe player who succeeds in placing three of their marks in \na horizontal, vertical, or diagonal row wins the game.\n")
+    randprint("\nPlayer 1 is X and Player 2 is O. Is that ok? (y/n) ")
+    playersetup = raw_input("")
+    if playersetup == "n":
+        players["1"] = "O"
+        players["2"] = "X"
+        print "\nOk. Player 1 is O and Player 2 is X."
+    elif playersetup == "y":
+        
+    else: 
+        randprint("\nYou entered an incorrect y/n selection. Player 1 is X and Player 2 is O.\n")
+    randprint("\nThe positions on the game board are numbered as follows:\n")
+    for i in range(1,10):
+        dic[str(i)] = str(i)
+        game[str(i)] = " "
+    drawboard(dic)
 #print the game board
-def printgame(var):
-    print "\nPlayer %s's turn:" % "1"
-    for i in range(size):
-        bhorizon()
-        bvert(var)
-    bhorizon()
 def drawboard(board):
-    print " --- --- ---"
-    print '| ' +board[0]+ ' | ' +board[1]+ ' | ' +board[2] + " |"
-    print " --- --- ---"
-    print '| ' +board[3]+ ' | ' +board[4]+ ' | ' +board[5] + " |"
-    print " --- --- ---"
-    print '| ' +board[6]+ ' | ' +board[7]+ ' | ' +board[8] + " |"
-    print " --- --- ---"
+    time.sleep(0.75)
+    print(" --- --- ---")
+    print("| " +board["1"]+ " | " +board["2"]+ " | " +board["3"] + " |")
+    print(" --- --- ---")
+    print("| " +board["4"]+ " | " +board["5"]+ " | " +board["6"] + " |")
+    print(" --- --- ---")
+    print("| " +board["7"]+ " | " +board["8"]+ " | " +board["9"] + " |")
+    print(" --- --- ---")
+#print for turn
+def taketurn(var):
+    time.sleep(0.5)
+    turn = whosturn(var)
+    print "\nPlayer %s's turn (%s's):" % (turn, players[turn])
+    drawboard(game)
+    move = raw_input("\nPlease select the position you want to mark: ")
+    while True:
+        if move == "Q": quit()
+        elif move not in game:
+            print "\nThe positions on the game board are numbered as follows:"
+            drawboard(dic)
+            move = raw_input("\nPlease select the position you want to mark, or enter Q to quit: ")
+            continue
+        elif game[move] != " ":
+            move = raw_input("\nThat position is taken! Please select again: ")
+            continue            
+        else:    
+            game[move] = players[turn]
+            break   
+#evaluate who's turn based on turn count
+def whosturn(var):
+    if var % 2 == 0:    
+        turn = "2"
+    else: 
+        turn = "1"
+    return turn
+#check if a winner
 def chkwin():
     #check rows
-    for i in range(3):
-        lst = []
-        for j in range(3):
-            lst.append(game[i][j])
-        if lst == wincom1 or lst == wincom2:
-            print lst
+    lst = []
+    for i in ["1","2","3"]:
+        lst.append(game[i])
+    if lst == wincom1 or lst == wincom2:
+        return "winner"
+    lst = []
+    for i in ["4","5","6"]:
+        lst.append(game[i])
+    if lst == wincom1 or lst == wincom2:
+        return "winner"
+    lst = []
+    for i in ["7","8","9"]:
+        lst.append(game[i])
+    if lst == wincom1 or lst == wincom2:
+        return "winner"
     #check columns
-    for i in range(3):
-        lst = []
-        for j in range(3):
-            lst.append(game[j][i])
-        if lst == wincom1 or lst == wincom2:
-            print lst
+    lst = []
+    for i in ["1","4","7"]:
+        lst.append(game[i])
+    if lst == wincom1 or lst == wincom2:
+        return "winner"
+    lst = []
+    for i in ["2","5","8"]:
+        lst.append(game[i])
+    if lst == wincom1 or lst == wincom2:
+        return "winner"
+    lst = []
+    for i in ["3","6","9"]:
+        lst.append(game[i])
+    if lst == wincom1 or lst == wincom2:
+        return "winner"
     #check diagonals
     lst = []
-    for i in range(3):
-        lst.append(game[i][i])
+    for i in ["1","5","9"]:
+        lst.append(game[i])
     if lst == wincom1 or lst == wincom2:
-        print lst
+        return "winner"
     lst = []
-    for i in range(3):
-        lst.append(game[i][(2-i)])
+    for i in ["3","5","7"]:
+        lst.append(game[i])
     if lst == wincom1 or lst == wincom2:
-        print lst
-drawboard([' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '])
+        return "winner"
+#handle the end of game, winner identified or tie
+def endgame(var,var1):
+    turn = whosturn(var1)
+    drawboard(game)
+    time.sleep(0.5)
+    if var == "winner":
+        print "Player %s won the game!" % turn
+    elif var == "tie":
+        print "The game was a tie."
+#used to make printed lines look typed
+def randprint(var):
+    for char in var:
+        sys.stdout.write(char)
+        sys.stdout.flush()
+        time.sleep((random.random() * 0.01))
+playgame(turncount)
