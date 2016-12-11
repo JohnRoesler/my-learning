6fe5c4cbc7c91ec91cf5063be0ea3d08c20b55a2
John Roesler <johnrroesler@gmail.com>
2016-12-11 15:39:06 -0600

    Updating composition package main file

--- UdemyJava/src/Composition/Main.java
+++ UdemyJava/src/Composition/Main.java
@@ -4,4 +4,18 @@ package src.Composition;
  * Created by Z001W4M on 12/11/2016.
  */
 public class Main {
+
+    public static void main(String[] args) {
+        Dimensions dimensions = new Dimensions(10,20,24);
+        Case theCase = new Case("220B","Dell","240", dimensions);
+        Monitor theMonitor = new Monitor("27in Beast", "Asus", 27, new Resolution(2540,1440));
+        Motherboard theMotherboard = new Motherboard("BJ-200", "Acer", 4, 6, "V2.45");
+        PC thePC = new PC(theCase,theMonitor,theMotherboard);
+
+        thePC.getMonitor().drawPixelAt(1500,1200,"Red");
+        thePC.getMotherboard().loadProgram("Run Windows XP");
+        thePC.getTheCase().pressPowerButton();
+    }
+
+
 }
