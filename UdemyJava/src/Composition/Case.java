6d9e3c2af90d4d92b2ac3800be9e96a53e1c21aa
John Roesler <johnrroesler@gmail.com>
2016-12-11 14:59:23 -0600

    Updated composition files

--- UdemyJava/src/Composition/Case.java
+++ UdemyJava/src/Composition/Case.java
@@ -4,5 +4,35 @@ package src.Composition;
  * Created by Z001W4M on 12/11/2016.
  */
 public class Case {
-    
+    private String model;
+    private String manufacturer;
+    private String powerSupply;
+    private Dimensions dimensions;
+
+    public Case(String model, String manufacturer, String powerSupply, Dimensions dimensions) {
+        this.model = model;
+        this.manufacturer = manufacturer;
+        this.powerSupply = powerSupply;
+        this.dimensions = dimensions;
+    }
+
+    private void pressPowerButton(){
+        System.out.println("Power button pressed");
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public String getManufacturer() {
+        return manufacturer;
+    }
+
+    public String getPowerSupply() {
+        return powerSupply;
+    }
+
+    public Dimensions getDimensions() {
+        return dimensions;
+    }
 }
---
+++ UdemyJava/src/Composition/PC.java
@@ -0,0 +1,29 @@
+package src.Composition;
+
+/**
+ * Created by Z001W4M on 12/11/2016.
+ */
+public class PC {
+
+    private Case theCase;
+    private Monitor monitor;
+    private Motherboard motherboard;
+
+    public PC(Case theCase, Monitor monitor, Motherboard motherboard) {
+        this.theCase = theCase;
+        this.monitor = monitor;
+        this.motherboard = motherboard;
+    }
+
+    public Case getTheCase() {
+        return theCase;
+    }
+
+    public Monitor getMonitor() {
+        return monitor;
+    }
+
+    public Motherboard getMotherboard() {
+        return motherboard;
+    }
+}
