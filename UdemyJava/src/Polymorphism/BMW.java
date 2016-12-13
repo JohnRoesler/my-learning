fd7c1e2cb7537972ab367dee4e0d6aac2b4d217b
John Roesler <johnrroesler@gmail.com>
2016-12-13 15:38:45 -0600

    Polymorphism example and challenge exercise

---
+++ UdemyJava/src/Polymorphism/BMW.java
@@ -0,0 +1,22 @@
+package src.Polymorphism;
+
+/**
+ * Created by Z001W4M on 12/13/2016.
+ */
+public class BMW extends Car{
+    public BMW(int cylinders, String name) {
+        super(cylinders, name, "BMW");
+    }
+
+    @Override
+    public void accelerate(int speed) {
+        super.accelerate(speed);
+        System.out.println(this.getMake());
+    }
+
+    @Override
+    public void brake() {
+        this.setSpeed(0);
+        System.out.println("The " + this.getMake() + " has come to a stop");
+    }
+}
---
+++ UdemyJava/src/Polymorphism/GMC.java
@@ -0,0 +1,22 @@
+package src.Polymorphism;
+
+/**
+ * Created by Z001W4M on 12/13/2016.
+ */
+public class GMC extends Car{
+    public GMC(int cylinders, String name) {
+        super(cylinders, name, "GMC");
+    }
+
+    @Override
+    public void accelerate(int speed) {
+        super.accelerate(speed);
+        System.out.println(this.getMake());
+    }
+
+    @Override
+    public void brake() {
+        this.setSpeed(0);
+        System.out.println("The " + this.getMake() + " has come to a stop");
+    }
+}
